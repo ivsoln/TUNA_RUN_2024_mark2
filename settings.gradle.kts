1f3ecbdc71364d9743ff2a584a882be9495c3948
Bhumi PH <bhumi.ph@gmail.com>
2024-06-28 15:35:01 +0700

    #bhumi 2024.6.28

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TUNA_RUN.2024"
+include(":app")
+ 
